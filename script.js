43a707633ba908a8ec06ab890cd50f53f4c26593
AnuragKalita32 <kalitaankur00@gmail.com>
2024-12-26 23:31:00 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('.surprise-btn').addEventListener('click', function () {
+    alert('🎉 Surprise! You are an amazing person, and today is all about you. Having a friend like is a real blessing I am very fortunate of this. Have a great day and may God bless you! SNIANG 🎉');
+});
